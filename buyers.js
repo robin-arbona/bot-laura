966e8244abd6a6ae557fd7122244e0e82dbde14a
Robin Arbona <robin@i-wantit.com>
2022-05-12 14:32:17 +0200

    add example to use puppeteer

--- buyers.js
+++ buyers.js
@@ -7,6 +7,16 @@ export const run = async () => {
     const page = await browser.newPage();
     await page.goto('https://example.com');
     await page.screenshot({ path: 'example.png' });
+
+    // await page.click('button.add-to-cart-btn.addToCart')
+
+
+    // await page.type('#username', 'foo@example.com');
+    // await page.type('#password', 'password');
+    // await page.keyboard.press('Enter');
+  
+    // await page.waitForNavigation();
+    // console.log('New Page URL:', page.url());
   
     await browser.close();
 }
